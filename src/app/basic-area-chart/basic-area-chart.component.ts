9b8271293441aa6e85c621a18676ee1956db620e
baby7305 <songhaibei7305@163.com>
2018-12-11 13:59:42 +0800

    先提交一部分

--- src/app/basic-area-chart/basic-area-chart.component.ts
+++ src/app/basic-area-chart/basic-area-chart.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicAreaChartComponent implements OnInit {
 
+  options: any;
+
   constructor() { }
 
   ngOnInit() {
+    this.options = {
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [{
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'line',
+        areaStyle: {}
+      }]
+    };
+
   }
 
 }
--- src/app/smoothed-line-chart/smoothed-line-chart.component.ts
+++ src/app/smoothed-line-chart/smoothed-line-chart.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SmoothedLineChartComponent implements OnInit {
 
+  options: any;
+
   constructor() { }
 
   ngOnInit() {
+    this.options = {
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [{
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'line',
+        smooth: true
+      }]
+    };
+
   }
 
 }
